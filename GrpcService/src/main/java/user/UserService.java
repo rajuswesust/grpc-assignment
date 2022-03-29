cf5f1a9061dcab7caf8952e4a7970f1e24e17c6e
raju singho <53114843+rajuswesust@users.noreply.github.com>
2022-03-29 16:16:35 +0600

    Update UserService.java

--- GrpcService/src/main/java/user/UserService.java
+++ GrpcService/src/main/java/user/UserService.java
@@ -16,13 +16,16 @@ public class UserService extends userImplBase{
 
         System.out.println("A new Client is registering...");
 
+        //Clients info
         String email = request.getEmail();
         String fullName = request.getFullName();
         String userName = request.getUsername();
         String password = request.getPassword();
 
+        //Clients information is storing in hashmap-> key=username. value=info object
         clients.put(userName, new Info(email, fullName, password));
 
+        //Response a message to the client
         User.APIResponse.Builder response = User.APIResponse.newBuilder();
         response.setResponseMessage("You Have been registered successfully. Please log in with you username and password...").setResponseCode(111);
 
@@ -35,11 +38,13 @@ public class UserService extends userImplBase{
 
         System.out.println("A user is logging in...");
 
+        //a user is logging in with username and password
         String username = request.getUsername();
         String password = request.getPassword();
 
         User.APIResponse.Builder response = User.APIResponse.newBuilder();
 
+        //printing users username and password
         System.out.println("["+ username + ", "+ password + "]");
         /***
         for(String i: clients.keySet()){
@@ -50,13 +55,16 @@ public class UserService extends userImplBase{
         if(clients.containsKey(username)){
             Info userInfo = clients.get(username);
             if(userInfo.password.equals(password)) {
+                System.out.println("User Found...\n");
                 response.setResponseCode(0).setResponseMessage("Login Successful...");
             }
             else{
+                System.out.println("User entered wrong password...\n");
                 response.setResponseCode(1).setResponseMessage("Wrong Password...");
             }
         }
         else{
+            System.out.println("User does not exists...\n");
             response.setResponseCode(100).setResponseMessage("No Such User Found...");
         }
 
